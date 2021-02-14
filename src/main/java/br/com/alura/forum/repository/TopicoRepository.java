3909845e16b7aa1490cc4877b54e84f90278f9d4
Danilo <danilosantossousa@gmail.com>
2021-02-14 08:51:16 -0300

    add comentário

--- src/main/java/br/com/alura/forum/repository/TopicoRepository.java
+++ src/main/java/br/com/alura/forum/repository/TopicoRepository.java
@@ -7,5 +7,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import br.com.alura.forum.modelo.Topico;
 
 public interface TopicoRepository extends JpaRepository<Topico, Long> {
+	// _ é usado para separar os relacionamentos entre entidades
 	List<Topico> findByCurso_Nome(String curso);
 }
